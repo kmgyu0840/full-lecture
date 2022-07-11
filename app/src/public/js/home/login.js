a0bac2a62e45ce13380793d0a0c363216f1d04fb
mgkim <kmgyu0840@naver.com>
2022-07-11 16:59:52 +0900

    DOM을 이용하여 HTML데이터 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-// 브라우저 개발자모드에서 Console 확인 시 hello
-console.log("hello");
+//HTML에서 입력된 데이터를 서버에서 제어할 수 있도록 DOM을 활용
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+//버튼이 클릭되면 id, pw를 서버로 가져오는
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {          //요청 데이터이므로 req 정의
+        id : id.value,     //id 데이터 가져오는
+        pw : pw.value,     //pw 데이터 가져오는
+    };
+    console.log(req);      //웹 개발자모드에 데이터 출력이되는지 확인
+}
