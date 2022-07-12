58a85c547869a992325e992785b1eae96f6b667a
mgkim <kmgyu0840@naver.com>
2022-07-12 12:44:19 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class UserStorage {
+    static #users = { //외부에서 다이렉트 접근하지 못하도록 #으로 은닉화
+        id : ["qwe", "asd", "zxc"],
+        pw : ["123", "456", "789"],
+        nm : ["가나", "다라", "마바"],
+    };
+
+    // 외부에서 데이터를 받을 수 있도록 메서드를 활용해야함
+    // 클래스에서 변수 접근하기 위해서는 static을 사용해야함
+    // 여러개의 데이터를 받기 위해서 ...fields 활용
+    // reduce함수 숙지
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUser = fields.reduce((newUser, field) => {
+            if (users.hasOwnProperty(field)) { //users에서 해당하는 키값이 있는지 있으면 true
+                newUser[field] = users[field];    //키와 값
+            }
+            return newUser;  
+        }, {});
+        return newUser;
+    }
+}
+
+// routes/home/home.ctrl.js에서 사용할 수 있도록 연결 역할
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,10 +1,7 @@
 "use strict";
 
-const users = {
-    id : ["qwe", "asd", "zxc"],
-    pw : ["123", "456", "789"],
-};
-
+// models/UserStorage를 사용하기 위해
+const UserStorage = require("../../models/UserStorage");
 
 // home, login은 뷰단을 내보내주는 역할로 output으로 정의
 const output = {
@@ -22,18 +19,23 @@ const process = {
         const id = req.body.id,
             pw = req.body.pw;
         
+        // UserStorage 데이터를 불러오기(받아오기) 위한 역할
+        // 지금처럼 직접 접근하는 것은 불가하도록 해야함 (UserStorage에서 #으로 은닉화함)
+        // getUsers 메서드 안에 원하는 데이터 컬럼명을 적어 불러올 수 있음
+        const users = UserStorage.getUsers("id", "pw");
+        
+        // response 정의하여 true, false 따로 정의
+        const response = {};
         if (users.id.includes(id)) {  //해당 id 확인 후 해당 index 불러오기
             const idx = users.id.indexOf(id);
             if (users.pw[idx] === pw) { //불러온 index 비밀번호만 일치하는지
-                return res.json({  //로그인에 성공하면 success:true 오브젝트를 json으로 프론트로 응답
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);  //로그인에 성공하면 success:true 오브젝트를 json으로 프론트로 응답
             }    
         }
-        return res.json({  //실패하면... 
-            success: false,
-            msg: "로그인에 실패하였습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하였습니다.";
+        return res.json(response);  //실패하면... 
     },
 };
 
